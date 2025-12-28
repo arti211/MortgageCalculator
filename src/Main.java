74314cd24a92035409daa62b13ecdc39fa19b0e4
arti pathare <artipathare@gmail.com>
2025-12-28 14:22:01 +0000

    Working refactored code

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,30 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("*****************************");
-        System.out.println("    MORTGAGE CALCULATOR");
-        System.out.println("*****************************");
+    final static int NUMBER_OF_MONTHS = 12;
+    final static int PERCENT = 100;
 
-        double principal = (int )readNumber(sc, "Principal: ",  1000,1_000_000 );
-        double annualInterestRate = (float) readNumber(sc, "Annual Interest Rate: ",  0,30 );
-        int periodOfYears = (int) readNumber(sc, "Period (Years): ",  1,30 );
+    public static void main(String[] args) {
+        double principal = (int )readNumber("Principal: ",  1000,1_000_000 );
+        double annualInterestRate = (float) readNumber("Annual Interest Rate: ",  0,30 );
+        int periodOfYears = (int) readNumber("Period (Years): ",  1,30 );
 
         //Without using a Class
         //var mortgage = new Mortgage(principal, periodOfYearsInMonths, monthlyInterestRate);
+        printMortgage(principal, annualInterestRate, periodOfYears);
+        printMortgageBalance(principal, annualInterestRate, periodOfYears);
+    }
+
+    private static void printMortgage(double principal, double annualInterestRate, int periodOfYears) {
+        System.out.println("\nMORTGAGE");
+        System.out.println("--------");
         double mortgage = calculateMortgage(principal, annualInterestRate, periodOfYears);
         System.out.println("Your monthly mortgage will be: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage));
     }
 
-    public static double readNumber(Scanner sc, String prompt, double min, double max) {
+    public static double readNumber(String prompt, double min, double max) {
         double value;
+        Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.print(prompt);
             value = sc.nextFloat();
@@ -32,14 +38,32 @@ public class Main {
     }
 
     public static double calculateMortgage(double principal, double annualInterestRate, int periodOfYears) {
-        final int NUMBER_OF_MONTHS = 12;
-        final int PERCENT = 100;
-
         double monthlyInterestRate = annualInterestRate/ (NUMBER_OF_MONTHS * PERCENT);
         int periodOfYearsInMonths = periodOfYears * 12;
 
         return principal* (monthlyInterestRate * Math.pow(1+monthlyInterestRate,periodOfYearsInMonths))
                 /(Math.pow(1+monthlyInterestRate,periodOfYearsInMonths) -1);
 
+    }
+
+    public static double calculatePaymentSchedule(double principal, double annualInterestRate, int periodOfYears, int monthsPaid){
+        double monthlyInterestRate = annualInterestRate/ (NUMBER_OF_MONTHS * PERCENT);
+        int periodOfYearsInMonths = periodOfYears * 12;
+
+        return principal *(Math.pow(1+monthlyInterestRate,periodOfYearsInMonths)-
+                Math.pow(1+monthlyInterestRate, monthsPaid))/
+                (Math.pow(1+monthlyInterestRate,periodOfYearsInMonths)-1);
+    }
+
+    private static void printMortgageBalance(double principal, double annualInterestRate, int periodOfYears) {
+        int numberOfMonthsPaid = 0;
+        System.out.println("\nPAYMENT SCHEDULE");
+        System.out.println("----------------");
+
+        for (int i = 0; i <= periodOfYears * NUMBER_OF_MONTHS ; i++) {
+            System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format
+                    (calculatePaymentSchedule(principal, annualInterestRate, periodOfYears, numberOfMonthsPaid)));
+            numberOfMonthsPaid++;
+        }
     }
 }
