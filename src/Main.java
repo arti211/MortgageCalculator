8e4331e320bd3bc69a07b352bfc7ab254a4b1b55
arti pathare <artipathare@gmail.com>
2025-12-28 11:10:07 +0000

    Working error validation

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,43 @@ public class Main {
     public static void main(String[] args) {
         final int NUMBER_OF_MONTHS = 12;
         final int PERCENT = 100;
+        int periodOfYears, periodOfYearsInMonths;
+        double annualInterestRate, monthlyInterestRate;
+        double principal;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("*****************************");
         System.out.println("    MORTGAGE CALCULATOR");
         System.out.println("*****************************");
 
-        System.out.print("Enter principal amount: ");
-        double principal = sc.nextDouble();
+        while (true) {
+            System.out.print("Enter principal amount: ");
+            principal = sc.nextDouble();
+            if (principal >= 1000 && principal <= 1_000_000) {
+                break;
+            }
+            System.out.println("Enter a value between 1000 and 1_000_000");
+        }
 
-        System.out.print ("Enter annual interest rate: ");
-        double annualInterestRate = sc.nextDouble();
-        double monthlyInterestRate = (double)annualInterestRate/ (NUMBER_OF_MONTHS * PERCENT);
+        while(true) {
+            System.out.print("Enter annual interest rate: ");
+            annualInterestRate = sc.nextDouble();
+            if (annualInterestRate >=0  && annualInterestRate <= 30) {
+                break;
+            }
+            System.out.println("Enter a value between 0 and 30");
+        }
+        monthlyInterestRate = (double)annualInterestRate/ (NUMBER_OF_MONTHS * PERCENT);
 
-        System.out.print("Enter period in years: ");
-        int periodOfYears = sc.nextInt();
-        int periodOfYearsInMonths = periodOfYears*12;
+        while (true) {
+            System.out.print("Enter period in years: ");
+            periodOfYears = sc.nextInt();
+            if  (periodOfYears >= 1 && periodOfYears <= 30) {
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+        periodOfYearsInMonths = periodOfYears * 12;
 
         var mortgage = new Mortgage(principal, periodOfYearsInMonths, monthlyInterestRate);
         System.out.println("Your monthly mortgage will be: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage.calculateMortgage()));
