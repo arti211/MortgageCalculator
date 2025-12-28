c8a7b3f145cf773d36c3e46a0195b044857301b1
arti pathare <artipathare@gmail.com>
2025-12-28 13:03:11 +0000

    Working refactored code

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final int NUMBER_OF_MONTHS = 12;
-        final int PERCENT = 100;
-        int periodOfYears, periodOfYearsInMonths;
-        double annualInterestRate, monthlyInterestRate;
-        double principal;
-
         Scanner sc = new Scanner(System.in);
         System.out.println("*****************************");
         System.out.println("    MORTGAGE CALCULATOR");
         System.out.println("*****************************");
 
+        double principal = (int )readNumber(sc, "Principal: ",  1000,1_000_000 );
+        double annualInterestRate = (float) readNumber(sc, "Annual Interest Rate: ",  0,30 );
+        int periodOfYears = (int) readNumber(sc, "Period (Years): ",  1,30 );
+
+        //Without using a Class
+        //var mortgage = new Mortgage(principal, periodOfYearsInMonths, monthlyInterestRate);
+        double mortgage = calculateMortgage(principal, annualInterestRate, periodOfYears);
+        System.out.println("Your monthly mortgage will be: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage));
+    }
+
+    public static double readNumber(Scanner sc, String prompt, double min, double max) {
+        double value;
         while (true) {
-            System.out.print("Enter principal amount: ");
-            principal = sc.nextDouble();
-            if (principal >= 1000 && principal <= 1_000_000) {
+            System.out.print(prompt);
+            value = sc.nextFloat();
+            if  (value >= min && value <= max)
                 break;
-            }
-            System.out.println("Enter a value between 1000 and 1_000_000");
+            System.out.println("Enter a value between " +  min + " and " + max );
         }
+        return value;
+    }
 
-        while(true) {
-            System.out.print("Enter annual interest rate: ");
-            annualInterestRate = sc.nextDouble();
-            if (annualInterestRate >=0  && annualInterestRate <= 30) {
-                break;
-            }
-            System.out.println("Enter a value between 0 and 30");
-        }
-        monthlyInterestRate = (double)annualInterestRate/ (NUMBER_OF_MONTHS * PERCENT);
+    public static double calculateMortgage(double principal, double annualInterestRate, int periodOfYears) {
+        final int NUMBER_OF_MONTHS = 12;
+        final int PERCENT = 100;
 
-        while (true) {
-            System.out.print("Enter period in years: ");
-            periodOfYears = sc.nextInt();
-            if  (periodOfYears >= 1 && periodOfYears <= 30) {
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-        }
-        periodOfYearsInMonths = periodOfYears * 12;
+        double monthlyInterestRate = annualInterestRate/ (NUMBER_OF_MONTHS * PERCENT);
+        int periodOfYearsInMonths = periodOfYears * 12;
 
-        var mortgage = new Mortgage(principal, periodOfYearsInMonths, monthlyInterestRate);
-        System.out.println("Your monthly mortgage will be: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage.calculateMortgage()));
+        return principal* (monthlyInterestRate * Math.pow(1+monthlyInterestRate,periodOfYearsInMonths))
+                /(Math.pow(1+monthlyInterestRate,periodOfYearsInMonths) -1);
+
     }
 }
